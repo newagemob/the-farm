8dc47fc7acf1ce37afac0249394ed36b843d89b4
newagemob <isaiah.wright@tutanota.com>
2022-12-01 14:31:46 -0600

    🧹 cleaning old Selenium functions with new API

--- engage.py
+++ engage.py
@@ -33,10 +33,6 @@ hashtags = [
     # # 'grad',
     # # 'classof',
 
-    # 'birthday',
-    # # 'birthdaygirl',
-    # # 'birthdayboy',
-
     # 'finance',
     # 'financetips',
     # 'frugal',
@@ -50,31 +46,37 @@ hashtags = [
     # 'sweepstakes',
     # 'win',
 
-    'technology',
-    'tech',
-    'technews',
-    'code',
-    'startup',
-    'entrepreneur',
+    # 'technology',
+    # 'tech',
+    # 'technews',
+    # 'code',
+    # 'startup',
+    # 'entrepreneur',
 
-    'travel',
-    'travelblogger',
-    'travelgram',
-    'travelphotography',
+    # 'travel',
+    # 'travelblogger',
+    # 'travelgram',
+    # 'travelphotography',
 
-    'christmas',
     'christmasgifts',
     'holidays',
     'holidayseason',
-    'holidayshopping',
+    'holidaygifts',
+    'gifts',
+    'giftideas',
+
+    'birthday',
+    'birthdaygifts',
+
+    'teacher',
+    'teachers',
+    'teacheroftheyear',
+    'teacherappreciation',
+    'teacherappreciationweek',
+    'teacherappreciationgifts',
 ]
 
 random_hashtags = [
-    'fitness',
-    'gym',
-    'workout',
-    'health',
-
     'fyp',
     'foryou',
 
@@ -84,10 +86,10 @@ random_hashtags = [
     'travelphotography',
     'travelphoto',
 
-    # 'landscape',
-    # 'nature',
-    # 'naturephotography',
-    # 'naturephoto',
+    'fitness',
+    'gym',
+    'workout',
+    'health',
 
     'whales',
     'whalewatching',
@@ -105,7 +107,7 @@ comments = [
     'Spicy 🌶️',
     '👨🏽 Very nice! 🇰🇿',
     '🌊🌊🌊',
-    ]
+]
 
 
 class InstagramBot:
@@ -115,9 +117,9 @@ class InstagramBot:
         # self.driver = webdriver.Chrome()
         self.driver = webdriver.Firefox()
 
-
     # Login to Instagram
     # TODO: store login info so we can deploy multiple tabs and run multiple bots without having to log in each time
+
     def login(self):
         driver = self.driver
         driver.get("https://www.instagram.com")
@@ -125,18 +127,20 @@ class InstagramBot:
         # login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
         # login_button.click()
         # time.sleep(2)
-        user_element = driver.find_element("xpath", "//input[@name='username']")
+        user_element = driver.find_element(
+            "xpath", "//input[@name='username']")
         user_element.clear()
         user_element.send_keys(self.username)
-        password_element = driver.find_element("xpath", "//input[@name='password']")
+        password_element = driver.find_element(
+            "xpath", "//input[@name='password']")
         password_element.clear()
         password_element.send_keys(self.password)
         password_element.send_keys(Keys.RETURN)
         print("Logged in as " + self.username)
         time.sleep(10)
 
-
     # Like a photo based on the list of hashtags
+
     def like_photo(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
@@ -161,49 +165,54 @@ class InstagramBot:
 
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
             # don't look sus
             driver.execute_script("window.scrollTo(0, 0);")
 
             try:
                 # like button is in span with the class name of "_aamw"
-                time.sleep(random.randint(10, 20)) # use this to avoid `429 error: too many requests` -- Instagram does not like bots, but we must persist against the thots
-                driver.find_element(By.XPATH, "//span[@class='_aamw']").click() # like the photo
+                # use this to avoid `429 error: too many requests` -- Instagram does not like bots, but we must persist against the thots
+                time.sleep(random.randint(10, 20))
+                driver.find_element(
+                    By.XPATH, "//span[@class='_aamw']").click()  # like the photo
                 print(f"liked {pic_href}")
             except Exception as e:
                 print(f"error liking {pic_href}")
 
-
     # Comment a relevant comment on a photo based on the list of hashtags
+
     def comment(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(5)
 
         for i in range(1, 3):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
 
         # searching for photos
-        hrefs = driver.find_elements_by_tag_name('a')
+        hrefs = driver.find_elements(By.TAG_NAME, "a")
         pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
         pic_hrefs = [href for href in pic_hrefs if hashtag in href]
 
         print(hashtag + ' photos: ' + str(len(pic_hrefs)))
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             try:
-                time.sleep(random.randint(2, 4))
+                time.sleep(random.randint(10, 20))
                 # find the comment input box
-                comment_input_box = driver.find_element(By.XPATH, "//textarea[@aria-label='Add a comment…']")
-                comment_input_box.click()
+                driver.find_element(
+                    By.XPATH, "//span[@class='_aamx']").click()
                 time.sleep(random.randint(2, 4))
                 # pick a random comment from the comments array
                 comment = random.choice(comments)
-                # write the comment in the comment input box
-                comment_input_box.send_keys(comment)
+                # write the comment in the comment input box -- textarea area-label="Add a comment..."
+                driver.find_element(By.XPATH, "//textarea[@aria-label='Add a comment...']").send_keys(comment)
                 time.sleep(random.randint(2, 4))
                 # find the post button and click it
                 post_button = driver.find_element(By.XPATH, "//button[@type='submit']")
@@ -213,8 +222,8 @@ class InstagramBot:
                 print(f"error commenting on {pic_href}")
                 print(e)
 
-
     # Filter mentions based on content, if the content is relevant, like the comment, leave a reply, like and the post
+
     def like_mentions(self):
         driver = self.driver
         driver.get("https://www.instagram.com/" + self.username)
@@ -224,22 +233,23 @@ class InstagramBot:
         # for each notification, check if it is a mention
         # if it is a mention, click on the notification and like the comment or photo
 
-
     # Like all comments on each post from a list of hashtags
+
     def like_comment(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(5)
         # like the first comment of each photo
 
-
     # Follow a user based on a list of hashtags
+
     def follow(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(5)
         for i in range(1, 3):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
         # searching for photos
         hrefs = driver.find_elements_by_tag_name('a')
@@ -248,22 +258,25 @@ class InstagramBot:
         print(hashtag + ' photos: ' + str(len(pic_hrefs)))
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             try:
                 time.sleep(random.randint(2, 4))
-                driver.find_element_by_xpath("//span[@aria-label='Follow']").click()
+                driver.find_element_by_xpath(
+                    "//span[@aria-label='Follow']").click()
                 time.sleep(random.randint(18, 25))
             except Exception as e:
                 time.sleep(2)
 
-
     # Unfollow users that don't follow you back
+
     def unfollow(self, hashtag):
         driver = self.driver
         driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
         time.sleep(5)
         for i in range(1, 3):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(2)
         # searching for photos
         hrefs = driver.find_elements_by_tag_name('a')
@@ -272,12 +285,15 @@ class InstagramBot:
         print(hashtag + ' photos: ' + str(len(pic_hrefs)))
         for pic_href in pic_hrefs:
             driver.get(pic_href)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             try:
                 time.sleep(random.randint(2, 4))
-                driver.find_element_by_xpath("//span[@aria-label='Following']").click()
+                driver.find_element_by_xpath(
+                    "//span[@aria-label='Following']").click()
                 time.sleep(random.randint(2, 4))
-                driver.find_element_by_xpath("//button[contains(text(), 'Unfollow')]").click()
+                driver.find_element_by_xpath(
+                    "//button[contains(text(), 'Unfollow')]").click()
                 time.sleep(random.randint(18, 25))
             except Exception as e:
                 time.sleep(2)
@@ -305,23 +321,21 @@ def follow():
     bot = InstagramBot(username, password)
     bot.login()
 
-    while True:
-        # actions
-        bot.follow('python')
-        # end actions
-        time.sleep(1)
+    # actions
+    for hashtag in hashtags:
+        bot.follow(hashtag)
+        time.sleep(5)
 
 
 def comment():
     bot = InstagramBot(username, password)
     bot.login()
 
-    while True:
-        # actions
-        bot.comment('python')
-        # end actions
-        time.sleep(1)
-
+    # actions
+    for hashtag in hashtags:
+        # loop through each hashtag and comment on each photo
+        bot.comment(hashtag)
+        time.sleep(5)
 
 def unfollow():
     bot = InstagramBot(username, password)
@@ -336,6 +350,6 @@ def unfollow():
 
 if __name__ == "__main__":
     like_photos()
-    # follow()
     # comment()
+    # follow()
     # unfollow()
